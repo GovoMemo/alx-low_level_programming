19c5e839b6514d5b53f244bfffd86d310a72f6de
GovoMemo <memorygovo@gmail.com>
2023-06-20 08:34:12 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet - print the alphabet in lowercases
+ * print_alphabet - print the alphabet in lower cases
  */
 
 void print_alphabet(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++) 
-
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 
-	_putchar('\n');
+	_putcher('\n');
 }
