281334536b2644d14e0a2193f96310d30f2a4c07
GovoMemo <memorygovo@gmail.com>
2023-06-20 22:36:29 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
-
 /**
- * main - Entry point
- * Return: 0
+ * main - print _putchar
+ * is written by mbah
+ * Return: Always 0
  */
 
 int main(void)
 {
-	-putchar('_');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
