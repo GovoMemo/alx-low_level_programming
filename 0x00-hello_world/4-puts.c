487e9deefb9fd3ca7f287fba3957ff90baea0e6f
GovoMemo <memorygovo@gmail.com>
2023-06-27 00:05:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle;
+ * main - prints a phrase 'programming is like building
+ * a multilingual puzzle'
  * Return: 0 if exited properly, non-zero otherwise;
  */
 int main(void)
