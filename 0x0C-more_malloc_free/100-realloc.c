c383dd64e2834bd7fa635cdf3089b276c44a62e9
GovoMemo <memorygovo@gmail.com>
2023-08-10 12:10:46 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,37 +8,38 @@
  * @ptr: input
  * @old_size: input
  * @new_size: input
- * Return: void
+ * Return: pointer to newly allocated memory block.
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *nptr;
+	char *ptr1;
+	char *old_ptr;
 	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
-
-	if (ptr == NULL)
+	if (new_size == 0 && ptr)
 	{
-		nptr = malloc(new_size);
-		if (nptr == NULL)
-			return (NULL);
-
-		return (nptr);
+		free(ptr);
+		return (NULL);
 	}
-	else
+	if (!ptr)
+		return (malloc(new_size));
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+	old_ptr = ptr;
+	if (new_size < old_size)
 	{
-		if (new_size == 0)
-		{
-			free(ptr);
-			return (NULL);
-		}
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
 	}
-	for (i = 0; i < old_size && i < new_size; i++)
+	if (new_size > old_size)
 	{
-		nptr[i] = ((char *) ptr)[i];
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
 	}
 	free(ptr);
-	return (nptr);
+	return (ptr1);
 }
